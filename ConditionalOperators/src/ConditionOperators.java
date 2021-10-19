5c9c81e2ca8892c647c14648ff75c77ffe46905b
IIPAIIOBEDNIK <pereponov_alexander@mail.ru>
2021-10-19 22:48:37 +0300

    Added first programs

---
+++ ConditionalOperators/src/ConditionOperators.java
@@ -0,0 +1,11 @@
+public class ConditionOperators {
+    public static void main(String[] args){
+        int temperatures = 25;
+        if (temperatures ==25) {
+            System.out.println("Кодиционер был включен");
+        }
+        else if (temperatures != 25){
+            System.out.println("Кондиционер выключен");
+        }
+    }
+}
---
+++ Variables/src/Variables.java
@@ -0,0 +1,8 @@
+public class Variables {
+    public static void main(String[] args){
+        int a = 5;
+        int b = 7;
+        int sum = a +b;
+        System.out.println("Сумма введенных чисел равна: " + sum);
+    }
+}
