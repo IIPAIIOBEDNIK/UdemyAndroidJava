def7f6f43375ded75c0666b385b689f04f59d178
IIPAIIOBEDNIK <pereponov_alexander@mail.ru>
2021-10-29 14:49:36 +0300

    Added programs

---
+++ OOP/src/lesson/Box.java
@@ -0,0 +1,9 @@
+package lesson;
+
+public class Box {
+    public static void main(String[] args) {
+        double widht;
+        double hight;
+        double lenght;
+    }
+}
---
+++ OOP/src/lesson/Main.java
@@ -0,0 +1,13 @@
+package lesson;
+
+public class Main {
+    public static void main(String[] args) {
+        Box box1 = new Box();
+        Box box2 = new Box();
+        Box box3 = new Box();
+
+
+
+
+    }
+}
